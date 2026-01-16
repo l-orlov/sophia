5826a046624036df742947f7424bfdf1508f60db
cebem1nt <mineewarik@gmail.com>
2026-01-16 18:58:47 -0300

    Finished initial design

--- web/landing.php
+++ web/landing.php
@@ -1,4 +1,4 @@
-<div class="hero">
+<div class="background hero">
     <?include 'include/header.php';?>
     <div class="headline">
         <h1>
@@ -7,9 +7,9 @@
     </div>
 </div>
 
-<div class="slide-1">
+<section id="_1">
     <div class="picture">
-        <img src="/img/robot.png" alt="robot" style="width: 550px;">
+        <img src="/img/robot.png" alt="robot" style="width: 559px; margin-top: -20px;">
     </div>
     <div class="content">
         <div class="block">
@@ -31,9 +31,9 @@
             <button>Conocé nuestro enfoque</button>
         </div>
     </div>
-</div>
+</section>
 
-<div class="slide-2">
+<section class="background" id="_2">
     <div class="content">
         <div class="text">
             <h1>Nuestras soluciones se adaptan a distintos sectores con desafíos específicos. Diseñamos estrategias tecnológicas a medida, alineadas a procesos y objetivos de cada organización</h1>
@@ -43,9 +43,9 @@
             <img src="/img/ico/arrow.svg" alt="arrow" style="margin-left: 18px;">
         </div>
     </div>
-</div>
+</section>
 
-<div class="slide-3">
+<section class="background" id="_3">
     <div class="infinite-slider">
         <div class="strip">
             <div class="track">
@@ -70,10 +70,10 @@
             <h1>Desarrollamos productos y plataformas propias que resuelven necesidades tecnológicas concretas y  se pueden implementar de forma independiente o integrada</h1>
         </div>
     </div>
-</div>
+</section>
 
-<div class="slide-4">
-    <div class="grid-container">
+<section id="_4">
+    <div class="background grid-container">
         <div class="container">
             <div class="headline">
                 <h1>Intelacs Energy</h1>
@@ -89,25 +89,140 @@
 
         <div class="container empty"></div>
     </div>
-</div>
+</section>
 
-<div class="slide-5">
-    <div class="container">
+<section class="background" id="_5">
+    <div class="text">
         <div class="headline">
             <h1>Nuestro enfoque integra estrategia, desarrollo tecnológico y ejecución para generar soluciones robustas y sostenibles</h1>
         </div>
     </div>
 
-    <div class="buttons">
-        <button>Diagnóstico del desafío</button>
-        <button>Arquitectura de solución</button>
-        <button>Desarrollo e integración</button>
-        <button>Implementación y operación</button>
-    </div>
+    <div class="buttons-contact">
+        <div class="buttons">
+            <button>Diagnóstico del desafío</button>
+            <button>Arquitectura de solución</button>
+            <button>Desarrollo e integración</button>
+            <button>Implementación y operación</button>
+        </div>
 
-    <div class="contact">
-        <button>Conocé nuestro proceso</button>
+        <div class="contact">
+            <button>Conocé nuestro proceso</button>
+        </div>
     </div>
-</div>
+</section>
 
-<!-- <script src="/js/landing.js"></script> -->
+<section id="_6">
+    <svg class="bg-circle" viewBox="0 0 200 200" aria-hidden="true">
+        <defs>
+            <linearGradient id="grad90" x1="0%" y1="0%" x2="0%" y2="100%" gradientUnits="userSpaceOnUse">
+                <stop offset="0%" stop-color="#2ED0E9"/>
+                <stop offset="100%" stop-color="#1A7583"/>
+            </linearGradient>
+        </defs>
+
+        <g class="static">
+            <line 
+                x1="-1000" y1="50%" x2="1000" y2="50%" fill="none"
+                stroke="url(#grad90)" 
+                stroke-width="1" 
+                stroke-linecap="butt"
+                stroke-dasharray="5 4"
+            />
+        </g>
+
+        <g class="rotating">
+            <circle 
+                cx="100" cy="100" r="90" fill="none"
+                stroke="url(#grad90)" 
+                stroke-width="2"
+                stroke-linecap="butt"
+                stroke-dasharray="5 4" 
+            />
+        </g>
+    </svg>
+
+    <div class="text">
+        <div class="headline">
+            <h1>I+D+I  INNOVACION  APLICADA CON  IMPACTO  REAL </h1>
+            <h2>En SophIA impulsamos Investigación, Desarrollo e Innovación para crear soluciones tecnológicas que respondan a desafíos actuales de los territorios y las organizacioness</h2>
+        </div>
+        <div class="btn-container">
+            <button>Conocer I+D+i</button>
+        </div>
+    </div>
+    <div class="previews">
+        <div class="background grid-container">
+            <div class="card">
+                <div class="container">
+                    <div class="block">
+                        <h1 class="block-header">Investigación</h1>
+                        <p class="block-text">Exploramos tendencias y metodologías (IA, datos, sensores) para detectar oportunidades concretas de mejora.</p>
+                    </div>
+                    
+                    <div class="decorators">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="12" cy="12" r="4.5" fill="#224A8A" stroke="white"/>
+                            <circle cx="12" cy="12" r="12" fill="#227E8A" fill-opacity="0.69"/>
+                            <circle cx="12" cy="12" r="11.5" stroke="white" stroke-opacity="0.83"/>
+                        </svg>
+
+
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="12" cy="12" r="4.5" fill="#224A8A" stroke="white"/>
+                            <circle cx="12" cy="12" r="11.5" stroke="white" stroke-opacity="0.83"/>
+                        </svg>
+
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="12" cy="12" r="4.5" fill="#224A8A" stroke="white"/>
+                            <circle cx="12" cy="12" r="11.5" stroke="white" stroke-opacity="0.83"/>
+                        </svg>
+                    </div>
+                </div>
+
+                <div class="container empty">
+                    <div class="block">
+
+                    </div>
+                </div>
+
+                <!-- TODO Hardcoded for now -->
+
+            </div>
+        </div>
+        <div class="content">
+            <img src="/img/preview/2.png" alt="tmp" style="height: 400px;">
+        </div>
+    </div>
+</section>
+
+<section id="_7">
+    <div class="content">
+        <div class="text">
+            <h1>TECNOLOGIAS QUE  IMPULSAN NUESTRAS SOLUCIONES</h1>
+            <h2>Construimos sobre tecnologías que permiten capturar, procesar y aprovechar datos en tiempo real para tomar mejores decisiones y automatizar procesos</h2>
+        </div>
+
+        <div class="btn-container">
+            <button>Explorá nuestras soluciones</button>
+        </div>
+    </div>
+</section>
+
+<section id="_8">
+    <div class="group">
+        <div class="desc left">
+            <h3>Analítica de datos</h3>
+            <h3>Software & Plataformas</h3>
+            <h3>Fintech y trazabilidad</h3>
+        </div>
+        <div class="robot">
+            <img src="/img/robot-2.png" alt="robot" style="width: 165px;">
+        </div>
+        <div class="desc right">
+            <h3>Internet de las Cosas (IoT)</h3>
+            <h3>Cloud & Edge Computing</h3>
+            <h3>Inteligencia Artificial</h3>
+        </div>
+    </div>
+</section>
